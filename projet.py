4b950a007f022d99c18e1341845f45860259d4bc
apierfitte <auguste.pierfitte@gmail.com>
2020-10-27 12:13:18 +0100

    question 7 terminée (auto-intersection)

--- projet.py
+++ projet.py
@@ -79,87 +79,62 @@ def level_curve(f, x0, y0, delta=0.1, N=1000, eps=eps):
         x0, y0 = x, y
     return res
 
-tableau = level_curve(f1, 0.3, 0.8)
-plt.plot(tableau[0], tableau[1])
-plt.show()
 
+def intersects(a, b):
+    p = [b[0][0]-a[0][0], b[0][1]-a[0][1]]
+    q = [a[1][0]-a[0][0], a[1][1]-a[0][1]]
+    r = [b[1][0]-b[0][0], b[1][1]-b[0][1]]
 
-# renvoie la position du point c par rapport à la droite AB
-# 1 <=> c est "au-dessus" de la droite 
-# -1 <=> c est "en-dessous"
-# a,b et c sont colinéaires
-# a,b et c sont supposés différents
-def side(a,b,c):
-    d = (c[1]-a[1])*(b[0]-a[0]) - (b[1]-a[1])*(c[0]-a[0])
-    return 1 if d > 0 else (-1 if d < 0 else 0)
+    t = (q[1]*p[0] - q[0]*p[1])/(q[0]*r[1] - q[1]*r[0]) \
+        if (q[0]*r[1] - q[1]*r[0]) != 0 \
+        else (q[1]*p[0] - q[0]*p[1])
+    u = (p[0] + t*r[0])/q[0] \
+        if q[0] != 0 \
+        else (p[1] + t*r[1])/q[1]
 
-# renvoie True si c est à l'intérieur du segment [a, b], False sinon
-# les points a, b et c sont supposés colinéaires
-def is_point_in_closed_segment(a, b, c):
-    """ Returns True if c is inside closed segment, False otherwise.
-        a, b, c are expected to be collinear
-    """
-    if a[0] < b[0]:
-        return a[0] <= c[0] and c[0] <= b[0]
-    if b[0] < a[0]:
-        return b[0] <= c[0] and c[0] <= a[0]
-
-    if a[1] < b[1]:
-        return a[1] <= c[1] and c[1] <= b[1]
-    if b[1] < a[1]:
-        return b[1] <= c[1] and c[1] <= a[1]
-
-    return a[0] == c[0] and a[1] == c[1]
-
-# Vérifie si les segments [a, b] et [c, d] s'intersectent
-def closed_segment_intersect(a,b,c,d):
-    if a == b:
-        return a == c or a == d
-    if c == d:
-        return c == a or c == b
-
-    s1 = side(a,b,c)
-    s2 = side(a,b,d)
-
-    # Tous les points sont colinéaires
-    if s1 == 0 and s2 == 0:
-        return \
-            is_point_in_closed_segment(a, b, c) or is_point_in_closed_segment(a, b, d) or \
-            is_point_in_closed_segment(c, d, a) or is_point_in_closed_segment(c, d, b)
-
-    # Ils ne se touchent pas et c et d sont du même côté de la droite AB
-    if s1 and s1 == s2:
-        return False
-
-    s1 = side(c,d,a)
-    s2 = side(c,d,b)
-
-    # idem
-    if s1 and s1 == s2:
-        return False
-
-    # arrivé ici, les segments s'intersectent
-    return True
+    return t >= 0 and t <= 1 and u >= 0 and u <= 1
 
 def level_curve_question_7(f, x0, y0, delta=0.1, N=1000, eps=eps):
     res = np.empty((2, N), dtype=float)
     c = f(x0, y0)
     # on calcule les bornes du premier segment
+    bornes = []
+    for i in range(2):
+        gradient = grad(f)(x0, y0)
+        nouveau_point = np.array([x0, y0]) + delta * gradient
+        x , y = Newton(fonction_level_curve(f, x0, y0, c, delta), nouveau_point[0], nouveau_point[1])
+        res[0][i], res[1][i] = x, y
+        bornes.append(np.array([x, y]))
+        x0, y0 = x, y
+    premier_segment = np.array([bornes[0], bornes[1]])
+    
+    # on calcule un nouveau point
     gradient = grad(f)(x0, y0)
     nouveau_point = np.array([x0, y0]) + delta * gradient
-    a , b = Newton(fonction_level_curve(f, x0, y0, c, delta), nouveau_point[0], nouveau_point[1])
-    x0, y0 = a, b
-    for i in range(1, N) :
+    x , y = Newton(fonction_level_curve(f, x0, y0, c, delta), nouveau_point[0], nouveau_point[1])
+    res[0][2], res[1][2] = x, y
+    bornes.append(np.array([x, y]))
+    point_prec = np.array([x,y])
+    x0, y0 = x, y
+
+    # puis on calcule les suivants
+    for i in range(3, N) :
         gradient = grad(f)(x0, y0)
         # on se place à un nouveau point "dans le sens de grad(f)"
         nouveau_point = np.array([x0, y0]) + delta * gradient
         # on trouve les nouvelles coordonnées, sur le cercle de centre (x0, y0) et de rayon delta
         x , y = Newton(fonction_level_curve(f, x0, y0, c, delta), nouveau_point[0], nouveau_point[1])
-        # test d'auto-intersection
-        if closed_segment_intersect(a, b, x, y) :
-            print("Auto-intersection")
-            return res[::,:i]
-
         res[0][i], res[1][i] = x, y
+        # test d'auto-intersection
+        dernier_segment = np.array([point_prec, np.array([x, y])])
+        if intersects(premier_segment, dernier_segment) :
+            print(f"Auto-intersection après {i} points.")
+            return res[:,:i]
+        point_prec = np.array([x, y])
         x0, y0 = x, y
     return res
+
+tableau = level_curve_question_7(f1, -0.5, 0.1)
+print(tableau)
+plt.plot(tableau[0], tableau[1])
+plt.show()
